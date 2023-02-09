98506e529d6d1b45cd27606c4b5d49e6b9bc1b87
Marco Abate <Wisheechia@gmail.com>
2023-02-09 21:41:23 +0000

    [BACKEND] Migrating to node typescript

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import bodyparser from "body-parser";
+import path from 'path';
+import dotenv from 'dotenv';
+import { connectDB } from "./mongo/connect";
+import { add } from "./mongo/schema/cocktail";
+
+const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
+dotenv.config();
+
+app.use(express.static(process.cwd()+"/../che-beviamo/"));
+
+app.get('/', function (req: Request,res: Response) {
+  console.log('GET /');
+  res.sendFile(path.resolve(__dirname+'/../che-beviamo/src/index.html'));
+});
+
+app.get('/api/get', function (req: Request,res: Response) {
+  
+});
+
+
+app.listen(port, () => {
+  let mongo = connectDB();
+  
+  console.log(`Server started::${port}`);
+});
