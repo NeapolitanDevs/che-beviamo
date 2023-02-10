781ba26212d03d180713486d932218c8e1c4306b
Giuseppe <giuseppenappo17@gmail.com>
2023-02-10 20:00:29 +0100

    add angular material

--- che-beviamo/src/app/app.component.spec.ts
+++ che-beviamo/src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'che-beviamo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('che-beviamo');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
