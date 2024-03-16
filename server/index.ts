a057f13756a35936dac6f1a5df4419cb59a3da3a
Marco <wisheechia@gmail.com>
2024-03-16 16:44:41 +0100

    provola

--- server/index.ts
+++ server/index.ts
@@ -12,10 +12,10 @@ dotenv.config();
 
 //app.use(express.static(process.cwd()+"/../che-beviamo/"));
 
-app.get('/', function (req: Request,res: Response) {
+/*app.get('/', function (req: Request,res: Response) {
   console.log('GET /');
   res.sendFile(path.resolve(__dirname+'/../che-beviamo/src/index.html'));
-});
+});*/
 
 app.use('/api/cocktail', cocktailRouter);
 app.use('/api/ingredient', ingredientRouter);
